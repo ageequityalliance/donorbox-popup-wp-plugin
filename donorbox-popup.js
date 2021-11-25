41a21f3a7cf7216168756a092e66bb6e4fd6b77e
Nate Gay <email@nategay.me>
2021-11-25 00:02:03 -0600

    Reenable additional linters

--- donorbox-popup.js
+++ donorbox-popup.js
@@ -1,74 +1,87 @@
+/**
+ * Donorbox Popup Modal Controller
+ *
+ * @package  donorbox-popup-for-wp
+ */
+
 (function (w, d) {
-    const frameID = 'donorbox_widget_frame';
-    const url = w.donorboxPopup.url
-    const daysUntilNextShowing = w.donorboxPopup.daysUntilNextShowing
+	const frameID              = 'donorbox_widget_frame';
+	const url                  = w.donorboxPopup.url
+	const daysUntilNextShowing = w.donorboxPopup.daysUntilNextShowing
 
-    if (url === undefined || url === "") {
-        console.error('donorbox popup missing url')
-    }
+	if (url === undefined || url === "") {
+		console.error( 'donorbox popup missing url' )
+	}
 
-    if (daysUntilNextShowing === undefined) {
-        console.error('donorbox popup missing daysUntilNextShowing')
-    }
+	if (daysUntilNextShowing === undefined) {
+		console.error( 'donorbox popup missing daysUntilNextShowing' )
+	}
 
-    let setCookie = (cName, expDays) => {
-        let date = new Date();
-        date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
-        const expires = "expires=" + date.toUTCString();
-        document.cookie = cName + "=seen; secure; " + expires + "; path=/";
-    }
+	let setCookie       = (cName, expDays) => {
+		let date        = new Date();
+		date.setTime( date.getTime() + (expDays * 24 * 60 * 60 * 1000) );
+		const expires   = "expires=" + date.toUTCString();
+		document.cookie = cName + "=seen; secure; " + expires + "; path=/";
+	}
 
-    let getCookie = (cName) => {
-        const name = cName + "=";
-        const cDecoded = decodeURIComponent(document.cookie); //to be careful
-        const cArr = cDecoded .split('; ');
-        let res;
-        cArr.forEach(val => {
-            if (val.indexOf(name) === 0) res = val.substring(name.length);
-        })
-        return res;
-    }
+	let getCookie      = (cName) => {
+		const name     = cName + "=";
+		const cDecoded = decodeURIComponent( document.cookie );
+		const cArr     = cDecoded .split( '; ' );
+		let res;
+		cArr.forEach(
+			val => {
+				if (val.indexOf( name ) === 0) {
+					res = val.substring( name.length );
+				}
+			}
+		)
+		return res;
+	}
 
-    let openTheModal = (e, url, daysUntilNextShowing) => {
-        e.preventDefault();       
-        var frame = d.createElement('iframe');
+	let openTheModal = (e, url, daysUntilNextShowing) => {
+		e.preventDefault();
+		var frame    = d.createElement( 'iframe' );
 
-        d.body.style.overflow = 'hidden'
-        frame.id = frameID;
-        frame.frameborder = 0;
-        frame.setAttribute('allowpaymentrequest', true);
-        frame.src = url;
-        frame.style.position = 'fixed';
-        frame.style.display = 'block';
-        frame.style.left = '0px';
-        frame.style.top = '0px';
-        frame.style.width = '100%';
-        frame.style.height = '100%';
-        frame.style.margin = '0px';
-        frame.style.padding = '0px';
-        frame.style.border = 'none';
-        frame.style.overflowX = 'hidden';
-        frame.style.overflowY = 'auto';
-        frame.style.visibility = 'visible';
-        frame.style.backgroundColor = 'transparent';
-        frame.style.zIndex = 2147483647;
-        d.body.appendChild(frame);
-        frame.focus();
+		d.body.style.overflow = 'hidden'
+		frame.id              = frameID;
+		frame.frameborder     = 0;
+		frame.setAttribute( 'allowpaymentrequest', true );
+		frame.src                   = url;
+		frame.style.position        = 'fixed';
+		frame.style.display         = 'block';
+		frame.style.left            = '0px';
+		frame.style.top             = '0px';
+		frame.style.width           = '100%';
+		frame.style.height          = '100%';
+		frame.style.margin          = '0px';
+		frame.style.padding         = '0px';
+		frame.style.border          = 'none';
+		frame.style.overflowX       = 'hidden';
+		frame.style.overflowY       = 'auto';
+		frame.style.visibility      = 'visible';
+		frame.style.backgroundColor = 'transparent';
+		frame.style.zIndex          = 2147483647;
+		d.body.appendChild( frame );
+		frame.focus();
 
-        setCookie(frameID, daysUntilNextShowing);
-    }
+		setCookie( frameID, daysUntilNextShowing );
+	}
 
-    // Listen for modal close event
-    w.addEventListener('message', (event) => {
-        if (typeof event.data == 'object' && event.data.from == 'dbox' && event.data.close === true) {
-            d.getElementById(frameID).parentNode.removeChild(d.getElementById(frameID));
-            d.body.style.overflow = 'auto'
-        }
-    })
+	// Listen for modal close event
+	w.addEventListener(
+		'message',
+		(event) => {
+			if (typeof event.data == 'object' && event.data.from == 'dbox' && event.data.close === true) {
+				d.getElementById( frameID ).parentNode.removeChild( d.getElementById( frameID ) );
+				d.body.style.overflow = 'auto'
+			}
+		}
+	)
 
-    w.onload = (event) => {
-        if (getCookie(frameID) === undefined) {
-            openTheModal(event, url, daysUntilNextShowing)
-        }
-    };
+	w.onload = (event) => {
+		if (getCookie( frameID ) === undefined) {
+			openTheModal( event, url, daysUntilNextShowing )
+		}
+	};
 }(window, document));
--- donorbox-popup.php
+++ donorbox-popup.php
@@ -26,8 +26,8 @@ defined( 'ABSPATH' ) || exit;
  * @return void
  */
 function enqueue_scripts(): void {
-    $plugin_version = '1.0.2';
-	$script_key = 'donorbox-popup-js';
+	$plugin_version = '1.0.2';
+	$script_key     = 'donorbox-popup-js';
 	wp_enqueue_script(
 		$script_key,
 		plugins_url( '/donorbox-popup.js', __FILE__ ),
@@ -35,13 +35,13 @@ function enqueue_scripts(): void {
 		$plugin_version,
 		true
 	);
-    $url_key = 'donorbox_popup_url';
-    $days_key = 'donorbox_popup_days';
-    $url = get_option($url_key, '');
-    $days = get_option($days_key, 14);
-    if ( '' !== $url ) {
-        $url .= '?modal=true';
-    }
+	$url_key  = 'donorbox_popup_url';
+	$days_key = 'donorbox_popup_days';
+	$url      = get_option( $url_key, '' );
+	$days     = get_option( $days_key, 14 );
+	if ( '' !== $url ) {
+		$url .= '?modal=true';
+	}
 	wp_localize_script(
 		$script_key,
 		'donorboxPopup',
@@ -59,54 +59,55 @@ add_action( 'wp_enqueue_scripts', 'DonorboxPopup\enqueue_scripts' );
  * @return void
  */
 function admin_page(): void {
-    $url_key = 'donorbox_popup_url';
-    $days_key = 'donorbox_popup_days';
-    if ( array_key_exists($url_key, $_POST) ) {
-        if ( check_admin_referer() ) {
-            update_option($url_key, strip_query_params(esc_url_raw(wp_unslash($_POST[ $url_key ]))), true);
-        }
-    }
-    if ( array_key_exists($days_key, $_POST) ) {
-        if ( check_admin_referer() ) {
-            update_option($days_key, sanitize_text_field(wp_unslash($_POST[ $days_key ])), true);
-        }
-    }
-?>
+	$url_key  = 'donorbox_popup_url';
+	$days_key = 'donorbox_popup_days';
+	if ( array_key_exists( $url_key, $_POST ) ) {
+		if ( check_admin_referer() ) {
+			update_option( $url_key, strip_query_params( esc_url_raw( wp_unslash( $_POST[ $url_key ] ) ) ), true );
+		}
+	}
+	if ( array_key_exists( $days_key, $_POST ) ) {
+		if ( check_admin_referer() ) {
+			update_option( $days_key, sanitize_text_field( wp_unslash( $_POST[ $days_key ] ) ), true );
+		}
+	}
+	?>
 <h1>Donorbox Popup plugin for WordPress</h1>
 <p>This plugin allows website owners to include a full screen donorbox form on page load.</p>
 <form method="post" style="padding-right:10px;">
-    <p>
-    <label for="<?php echo sanitize_key($url_key); ?>" style="font-weight:600;">Donorbox campaign url:</label>
-    <input 
-        type="url"
-        id="<?php echo sanitize_key($url_key); ?>"
-        name="<?php echo sanitize_key($url_key); ?>"
-        value="<?php echo esc_url(get_option($url_key, '')); ?>"
-        placeholder="https://donorbox.org/my-campaign" 
-        style="width:100%;"></p>
-    <p>A visitor to your site will see the form on first page load. When they close the form, it will not reappear for the selected number of days.</p>
-    <p>
-    <label for="<?php echo sanitize_key($days_key); ?>" style="font-weight:600;">Days until next popup:</label>
-    <input
-        type="number"
-        id="<?php echo sanitize_key($days_key); ?>"
-        name="<?php echo sanitize_key($days_key); ?>"
-        value="<?php echo sanitize_key(get_option($days_key, 14)); ?>"
-        min="1"
-        step="1"></p>
-    <input type="submit" value="Submit">
-    <?php wp_nonce_field(); ?>
+	<p>
+	<label for="<?php echo sanitize_key( $url_key ); ?>" style="font-weight:600;">Donorbox campaign url:</label>
+	<input 
+		type="url"
+		id="<?php echo sanitize_key( $url_key ); ?>"
+		name="<?php echo sanitize_key( $url_key ); ?>"
+		value="<?php echo esc_url( get_option( $url_key, '' ) ); ?>"
+		placeholder="https://donorbox.org/my-campaign" 
+		style="width:100%;"></p>
+	<p>A visitor to your site will see the form on first page load. When they close the form, it will not reappear for the selected number of days.</p>
+	<p>
+	<label for="<?php echo sanitize_key( $days_key ); ?>" style="font-weight:600;">Days until next popup:</label>
+	<input
+		type="number"
+		id="<?php echo sanitize_key( $days_key ); ?>"
+		name="<?php echo sanitize_key( $days_key ); ?>"
+		value="<?php echo sanitize_key( get_option( $days_key, 14 ) ); ?>"
+		min="1"
+		step="1"></p>
+	<input type="submit" value="Submit">
+	<?php wp_nonce_field(); ?>
 </form>
-<?php
+	<?php
 }
 
 /**
  * Stip Query Params From URL
  *
+ * @param string $url Some URL
  * @return string
  */
 function strip_query_params( string $url ): string {
-    return substr($url, 0, strpos($url, "?")) ?: $url;
+	return substr( $url, 0, strpos( $url, '?' ) ) ?: $url;
 }
 
 /**
@@ -114,7 +115,7 @@ function strip_query_params( string $url ): string {
  *
  * @return void
  */
-function admin_menu(): void {    
-    add_menu_page('Donorbox Popup', 'Donorbox Popup', 'manage_options', 'donorbox_popup', 'DonorboxPopup\admin_page', 'dashicons-money-alt'); 
+function admin_menu(): void {
+	add_menu_page( 'Donorbox Popup', 'Donorbox Popup', 'manage_options', 'donorbox_popup', 'DonorboxPopup\admin_page', 'dashicons-money-alt' );
 }
-add_action('admin_menu', 'DonorboxPopup\admin_menu');
+add_action( 'admin_menu', 'DonorboxPopup\admin_menu' );
